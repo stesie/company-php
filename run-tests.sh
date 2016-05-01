ecc77d588ac03a20e23cfaa0b20a195f33a2c2fa
Stefan Siegl <stesie@brokenpipe.de>
2016-05-01 10:08:20 +0200

    run emacs --quick

--- run-tests.sh
+++ run-tests.sh
@@ -4,7 +4,7 @@ RV=0
 
 cd tests
 for file in *.el; do
-  emacs -batch  -l package -f package-initialize -l ert  -L "$PWD"/.. -L "$PWD" -l "$file"  -f ert-run-tests-batch-and-exit || RV=1
+  emacs -Q -batch  -l package -f package-initialize -l ert  -L "$PWD"/.. -L "$PWD" -l "$file"  -f ert-run-tests-batch-and-exit || RV=1
 done
 
 exit $RV
