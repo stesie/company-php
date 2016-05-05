26ae9014c382ae73b4096b1141b3ca816ff02c6c
Stefan Siegl <stesie@brokenpipe.de>
2016-05-05 12:55:39 +0200

    Import atom-autocomplete-php as submodule + generate tmp.php

--- company-php.el
+++ company-php.el
@@ -7,18 +7,38 @@
   :group 'php)
 
 ;;;###autoload
-(defcustom company-php-program
+(defcustom company-php--php-program
   "/usr/bin/php"
   "Default PHP program"
   :type 'string
   :group 'company-php)
 
-(defvar company-php-external-helper-dir
+(defcustom company-php--composer-program
+  "/usr/bin/composer"
+  "Default PHP Composer program"
+  :type 'string
+  :group 'company-php)
+
+(defcustom company-php--autoload-files
+  '("vendor/autoload.php" "autoload.php")
+  "Default Autoload files"
+  :type 'string
+  :group 'company-php)
+
+(defcustom company-php--classmap-files
+  '("vendor/composer/autoload_classmap.php" "autoload/ezp_kernel.php")
+  "Default Classmap files"
+  :type 'string
+  :group 'company-php)
+
+
+
+(defvar company-php--external-helper-dir
   (concat
    (file-name-directory
     (or load-file-name
 	(buffer-file-name)))
-   "php")
+   "atom-autocomplete-php/php")
   "Path to the external PHP helper")
 
 (defvar company-php-project-path nil
@@ -32,19 +52,27 @@
 	 (json-key-type 'string)
 	 (index-file-name
 	  (concat
-	   company-php-external-helper-dir "/../indexes/"
+	   company-php--external-helper-dir "/../indexes/"
 	   (secure-hash 'md5 company-php-project-path)
 	   "/index." name ".json")))
     (json-read-file index-file-name)))
 
 (defun company-php--run-helper (method &rest args)
   "Run external PHP helper"
+  (with-temp-file (concat company-php--external-helper-dir "/tmp.php")
+    (insert "<?php "
+	    "$config = array("
+	    "'composer' => '" company-php--composer-program "',"
+	    "'php' => '" company-php--php-program "',"
+	    "'autoload' => " (company-php--insert-array company-php--autoload-files) ","
+	    "'classmap' => " (company-php--insert-array company-php--classmap-files) ","
+	    ");"))
   (let* ((shell-result (shell-command-to-string
 			(combine-and-quote-strings
 			 (append
 			  (list
 			   company-php-program
-			   (concat company-php-external-helper-dir "/parser.php")
+			   (concat company-php--external-helper-dir "/parser.php")
 			   company-php-project-path
 			   (concat "--" method))
 			  args))))
@@ -52,6 +80,14 @@
 	 (json-false nil)
 	 (json-key-type 'string))
     (json-read-from-string shell-result)))
+
+(defun company-php--insert-array (array)
+  "Write PHP Array with strings from array"
+  (apply
+   #'concat
+   (append '("array(")
+	   (mapcar (lambda (elm) (concat "'" elm "',")) array)
+	   '(")"))))
 
 
 (provide 'company-php)
