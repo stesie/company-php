03809fc4c6f6b2a0361aeb8ea7e06e4327f3258e
Stefan Siegl <stesie@brokenpipe.de>
2016-05-05 13:08:36 +0200

    Add copyright/license headers to all el files

--- company-php.el
+++ company-php.el
@@ -1,3 +1,35 @@
+;;; company-php.el --- Company (auto completion) backend for PHP
+
+;; Copyright (C) 2016 Stefan Siegl
+
+;; Author: Stefan Siegl <stesie@brokenpipe.de>
+;; URL: https://github.com/stesie/company-php
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24") (cl-lib "0.5"))
+
+(defconst company-php-version-number "0.0.1"
+  "company-php version number.")
+
+(defconst company-php-modified "2016-05-05"
+  "company-php build date.")
+
+;;; License
+
+;; This file is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this file; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
 (require 'company)
 (require 'cl-lib)
 
@@ -9,25 +41,29 @@
 ;;;###autoload
 (defcustom company-php--php-program
   "/usr/bin/php"
-  "Default PHP program"
+  "Path to PHP program to run composer and reflection helper with."
   :type 'string
   :group 'company-php)
 
 (defcustom company-php--composer-program
   "/usr/bin/composer"
-  "Default PHP Composer program"
+  "company-php depends on composer to generate classmap files.  Specify the
+path to your composer bin (e.g. bin/composer, composer.phar, /usr/bin/composer)."
   :type 'string
   :group 'company-php)
 
 (defcustom company-php--autoload-files
   '("vendor/autoload.php" "autoload.php")
-  "Default Autoload files"
+  "List of relative paths to the files of autoload.php from composer.
+Usually this is just \"vendor/autoload.php\" for composer projects, but you may
+specify multiple paths if you have different paths for some projects."
   :type 'string
   :group 'company-php)
 
 (defcustom company-php--classmap-files
   '("vendor/composer/autoload_classmap.php" "autoload/ezp_kernel.php")
-  "Default Classmap files"
+  "List of relative paths to files that contain a classmap (array with \"className\" => \"fileName\").
+By default on composer it's vendor/composer/autoload_classmap.php"
   :type 'string
   :group 'company-php)
 
